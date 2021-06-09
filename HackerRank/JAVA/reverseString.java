dbad8773f41408acb1f54da27d56f6d68da02332
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2021-06-09 21:07:41 +0530

    String reverse

---
+++ HackerRank/JAVA/reverseString.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        int i=0;
+        int j=A.length()-1;
+        // while(A.charAt(i)==A.charAt(j) && (i++)<=(j--));
+        //     if(i>=j){
+        //         System.out.println("Yes");
+        //     }
+        //     else{
+        //         System.out.println("No");
+        //     }
+        while(A.charAt(i)==A.charAt(j) && i++<j--);
+        System.out.println((i>=j)?"Yes":"No");
+        
+        }
+    }
+    
+        
+    
+
+
+
+
