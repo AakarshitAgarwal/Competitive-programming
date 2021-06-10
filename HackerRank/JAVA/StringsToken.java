f23fb5085124095a413d6b34245967aa7d50a222
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2021-06-10 17:27:33 +0530

    Sting Token

--- HackerRank/JAVA/StringsToken.java
+++ HackerRank/JAVA/StringsToken.java
@@ -19,3 +19,39 @@ public class Solution {
     }
 }
 
+// All test case pass solution
+
+/*
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        // Write your code here.
+        
+        if(!scan.hasNext()){
+            System.out.println("0");
+            return;
+        }
+        
+        String s = scan.nextLine();
+        scan.close();
+        
+                String [] s2=s.trim().split("[!,?._'@\\s]+");
+                System.out.println(s2.length);
+            for (int i=0;i<s2.length;i++){
+                System.out.println(s2[i]);
+            }
+        
+        
+        scan.close();
+    }
+}
+
+
+
+*/
