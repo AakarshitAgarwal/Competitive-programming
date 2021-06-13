6a2929d01da0e90af900e8da13985aeba6aa7cbd
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2021-06-13 17:26:52 +0530

    Method overriding

---
+++ HackerRank/JAVA/methodoverriding.java
@@ -0,0 +1,16 @@
+//solution of method overriding 
+
+    // Write your overridden getNumberOfTeamMembers method here
+    @Override
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has 11 players in " + getName() );
+    }
+
+  
+
+    //And a solution of Method overrding 2 is
+
+    
+        //super.define_me();
+		String temp=super.define_me(); //Fix this line
+
