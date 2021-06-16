1358ac31495ad4795fdb3f1905c20299880c96d7
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2021-06-16 18:20:50 +0530

    DiagonalDifference

---
+++ HackerRank/Problem Solving/Algorithms/Warmup/Diagonnaldifference.java
@@ -0,0 +1,11 @@
+public static int diagonalDifference(List<List<Integer>> arr) {
+    // Write your code here
+    int leftdiagonal=0;
+    int rightdiagonal=0;
+    for (int i=0;i<arr.size();i++){
+        leftdiagonal = leftdiagonal+arr.get(i).get(i);
+        rightdiagonal =rightdiagonal + arr.get(i).get(arr.size()-i-1);  //for left diagonal iteration
+    
+    }
+        return Math.abs(leftdiagonal-rightdiagonal);
+    }
