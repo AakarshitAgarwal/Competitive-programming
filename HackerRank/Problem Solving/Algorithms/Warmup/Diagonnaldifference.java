d42d6ddf7f2cc95a5449c964e293889d529066ec
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2021-06-20 16:24:14 +0530

    Logic to get RigthDiagonal iteration

--- HackerRank/Problem Solving/Algorithms/Warmup/Diagonnaldifference.java
+++ HackerRank/Problem Solving/Algorithms/Warmup/Diagonnaldifference.java
@@ -4,7 +4,7 @@ public static int diagonalDifference(List<List<Integer>> arr) {
     int rightdiagonal=0;
     for (int i=0;i<arr.size();i++){
         leftdiagonal = leftdiagonal+arr.get(i).get(i);
-        rightdiagonal =rightdiagonal + arr.get(i).get(arr.size()-i-1);  //for left diagonal iteration
+        rightdiagonal =rightdiagonal + arr.get(i).get(arr.size()-i-1);  //for Right diagonal iteration
     
     }
         return Math.abs(leftdiagonal-rightdiagonal);
