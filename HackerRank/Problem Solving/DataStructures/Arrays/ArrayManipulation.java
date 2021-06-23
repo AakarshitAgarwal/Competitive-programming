2c01add52d0626a34b397dc2bf8a0c361cd3338c
AakarshitAgarwal <talktoshanuagarwal@gmail.com>
2021-06-23 22:03:44 +0530

    Some Array Problems

---
+++ HackerRank/Problem Solving/DataStructures/Arrays/ArrayManipulation.java
@@ -0,0 +1,43 @@
+class Result {
+
+    /*
+     * Complete the 'rotateLeft' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER d
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static List<Integer> rotateLeft(int d, List<Integer> arr) {
+    // Write your code here
+    int size=arr.size();
+    int newarr[]=new int[size];
+
+    for(int i=0;i<size;i++){
+        if(d>i){
+            newarr[size-d+i]=arr.get(i);
+        }
+        else{
+            newarr[i-d]=arr.get(i);
+        }
+    }
+    // for(int i=0;i<size;i++){
+    //     System.out.print(newarr[i]+" ");
+    // }
+    // List<Integer> list =new ArrayList<Integer>(Arrays.asList(newarr));
+    // List<Integer> list =ArrayList<Integer>(Arrays.asList(newarr));
+    
+    // List<Integer> list =new ArrayList<Integer>();
+    // Collections.addAll(list,newarr);
+    // return list;
+    List<Integer> list =new ArrayList<Integer>(size);
+    for(int i:newarr){
+        list.add(i);
+    }
+    
+        return list;
+    
+    }
+
+}
